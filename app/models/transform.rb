3dde95c71b79a30ab377ca7778d422455402cf1a
triggerwoods <keithflint@inbox.ru>
2011-07-20 20:25:23 +0400

    global2

--- app/models/transform.rb
+++ app/models/transform.rb
@@ -2,23 +2,22 @@ class Transform
 
 def initialize
  @d = {}
- @aliases = {:+ => :plus,
-       :* => :times,
-       :/ => :divide,
-       :- => :minus,
-       :"=" => :eq,
-       :<= => :leq,
-       :>= => :geq,
-       :"<>" => :neq,
-       :"=!" => :neq,
-	:< => :lt,
-	:> => :gt,}
+ #@aliases = {:+ => :plus,
+  #     :* => :times,
+   #    :/ => :divide,
+  #     :- => :minus,
+  #     :"=" => :eq,
+#       :<= => :leq,
+ #      :>= => :geq,
+  #     :"<>" => :neq,
+   #    :"=!" => :neq,
+#	:< => :lt,
+#	:> => :gt,}
 end
 
 
 
 def input(arg)
-
  if arg.is_a?(Symbol)
     arg
  else 
@@ -26,6 +25,7 @@ def input(arg)
  end
 end
 
+=begin
 def getoper(arg)
 if @aliases[arg.to_sym].nil?
    " <#{arg}/>"
@@ -34,17 +34,11 @@ else
 end
   
 end
-
+=end
 
 def apply(operator, *args)
  result = ['<apply>']
-
- if operator.is_a?(String)
-    result << getoper(operator)
- else
-    result << operator
- end
-
+ result << operator
  args.each do |arg|
    result << input(arg)
  end
@@ -52,33 +46,100 @@ def apply(operator, *args)
   result << '</apply>'
 end
 
+def plus(*args)
+ apply('<plus/>',args) 
+end
 
-def interval(str,*args)
- cl = []
+def factorial(arg)
+ apply('<factorial/>',arg) 
+end
 
- if str[0] == '('
-    cl[0] = 'open'
- else 
-    cl[0] = 'closed'
- end
+def divide(arg1,arg2)
+ apply('<divide/>',arg1,arg2)
+end
 
- if str[1] == ')'
-    cl[1] = 'open'
- else
-    cl[1] = 'closed'
- end
+def minus(arg1,arg2 = nil)
+ arr = [arg1,arg2].compact
+ apply('<minus/>',*arr)
+end
 
- if cl[0] == cl[1] 
-    cl[1] = ''
- else 
-    cl[1] = '-'+cl[1]
- end
+def power(arg1,arg2) 
+ apply('<power/>',arg1,arg2) 
+end
 
- result = ['<interval closure="'+cl[0]+cl[1]+'">']
- args.each do |arg|
-   result << input(arg)
- end
- 
+def sqr(arg)
+ apply('<power/>',arg,2)
+end
+
+def rem(arg1,arg2)
+ apply('<rem/>',arg1,arg2)
+end
+
+def not(arg)
+ apply('<not/>',arg)
+end
+
+def implies(arg1,arg2)
+ apply('<implies/>',arg1,arg2)
+end
+
+def abs(arg)
+ apply('<abs/>',arg)
+end
+
+def floor(arg)
+ apply('<floor/>',arg)
+end
+
+def ceiling(arg)
+ apply('<ceiling/>',arg)
+end
+
+def neq(arg1,arg2)
+ apply('<neq/>',arg1,arg2)
+end
+
+def equivalent(arg1,arg2)
+ apply('<equivalent/>',arg1,arg2)
+end
+
+
+
+
+
+
+def interval(arg1,arg2,closure={:lb => :closed, :ub => :closed})
+
+# cl = []
+
+# if str[0] == '('
+#    cl[0] = 'open'
+# else 
+#    cl[0] = 'closed'
+# end
+
+# if str[1] == ')'
+#    cl[1] = 'open'
+# else
+#    cl[1] = 'closed'
+# end
+
+# if cl[0] == cl[1] 
+#    cl[1] = ''
+# else 
+#    cl[1] = '-'+cl[1]
+# end
+
+
+if closure[:lb] == closure[:ub]
+   str = closure[:lb]
+else
+   str = closure.values.join('-') 
+end
+
+ result = ['<interval closure="'+str+'">']
+ result << input(arg1)
+ result << input(arg2) 
  result << '</interval>'
 end
 
