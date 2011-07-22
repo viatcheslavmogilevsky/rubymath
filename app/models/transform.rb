8980bc921d282e6832f9939aaaea9ad32eec42c3
triggerwoods <keithflint@inbox.ru>
2011-07-22 20:21:12 +0400

    spos

--- app/models/transform.rb
+++ app/models/transform.rb
@@ -4,27 +4,44 @@ def initialize
  @d = {}
 end
 
-
-
-
-
 def getd
  @d 
 end
 
 def method_missing(method_name,*args,&block)
-	if method_name[-3..-1] == '_is'
-         nd = {}
-         nd[:name] = args[0]
-         nd[:operands] = args[1]
-         nd[:params] = args[2]
-	 @d[method_name[0..-4].to_sym] = nd
-        else 
-          raise NoMethodError    
-        end
+ if method_name[-3..-1] == '_is'
+    nd = {}
+    nd[:name] = args[0]
+    nd[:operands] = args[1]
+    nd[:params] = args[2]
+    @d[method_name[0..-4].to_sym] = nd
+ else 
+    raise NoMethodError    
+ end
 end
 
+def spos
+ rec @d[:main]
+end
 
+def rec(arg)
+ result = "#{arg[:name]}("
+ arg[:operands].each do |elem|
+  #result += "," if flag
+  if elem.is_a?(Symbol)
+     unless @d[elem].nil?
+       str = rec(@d[elem])
+       result += str.to_s
+     else
+       result += elem.to_s
+     end
+  else
+   result += elem.to_s
+  end
+  #flag = true
+ end
+ result += ")"
+end
 
 
 end
